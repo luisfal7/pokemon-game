784161f0bda57cef8b367bf283fd2e701e089b97
Luis Falconi <luism.falconi@gmail.com>
2022-11-23 14:43:40 -0300

    modif tree project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <h1>¿Quién este pokémon?</h1>
-  <pokemon-picture :pokemonId="140" :showPokemon="true"/>
-  <pokemon-options />
+  <pokemon-page />
 </template>
 
 <script>
-import PokemonOptions from './components/PokemonOptions.vue'
-import PokemonPicture from './components/PokemonPicture.vue'
 
-import getPokemonOptions from '@/helpers/getPokemonOptions'
+import PokemonPage from './pages/PokemonPage.vue'
 
 export default {
   name: 'App',
   components: {
-    PokemonOptions,
-    PokemonPicture
+    PokemonPage
   }
 }
 </script>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,13 +1,21 @@
 <template>
   <h1>¿Quién es este pokémon?</h1>
+  <pokemon-picture :pokemonId="140" :showPokemon="true" />
+  <pokemon-options />
 </template>
 
 <script>
-export default {
+import PokemonOptions from "@/components/PokemonOptions.vue";
+import PokemonPicture from "@/components/PokemonPicture.vue";
 
-}
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+export default {
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
